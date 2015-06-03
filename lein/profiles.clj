7a0226111b7a967dccb2831d1b8d08d2c9f385c0
Chris Blom <chris.blom@millmobile.com>
2015-06-03 17:09:51 +0200

    clean up leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,88 +4,59 @@
 
  :cider
  {:plugins
-  [[cider/cider-nrepl "0.8.2"]  ]
+  [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ]
   :dependencies
-  [[org.clojure/tools.nrepl "0.2.6"]]
+  [[org.clojure/tools.nrepl "0.2.7"]]
   :jvm-opts ["-Dapple.awt.UIElement=true"] ; hide java icon in OSX dock
   }
 
  :power
  {
   :plugins
-  [[lein-midje "3.1.3"] ; better testing
-   [cider/cider-nrepl "0.8.2"] ; cider repl integration
- ;  [refactor-nrepl "0.1.0"] ; clj-refactor integration
-   [lein-ns-dep-graph "0.1.0-SNAPSHOT" :exclusions [org.clojure/clojure]] ; namespace dependency graphs
-   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]] ; static code analysis
-   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]] ; linter
-   [lein-drip "0.1.1-SNAPSHOT"] ; jvm launch accelarator
-   [lein-ancient "0.5.5"  :exclusions [org.clojure/clojure]] ; dependency update checker
-   [lein-localrepo "0.5.3"] ; installs artifacts in local maven repo
+  [[lein-midje "3.1.3"] ; nicer testing
+                                        ;[refactor-nrepl "1.0.2-SNAPSHOT"]
+   [org.clojure/core.typed "0.2.72"]
+   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+   [jonase/kibit "0.0.8"]
+   [cider/cider-nrepl "0.9.0-SNAPSHOT"] ; cider repl integration
+   ;; [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]] ; static code analysis
+   ;; [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]] ; linter
+   ;; [lein-ancient "0.5.5"  :exclusions [org.clojure/clojure]] ; dependency update checker
+   ;; [lein-localrepo "0.5.3" :exclusions [org.clojure/clojure]] ; installs artifacts in local maven repo
    ]
 
   :jvm-opts ["-Dapple.awt.UIElement=true"] ; hide java icon in OSX dock
 
   :dependencies
-  [[org.clojure/tools.nrepl "0.2.6"]
+  [[org.clojure/tools.nrepl "0.2.7"]
    [org.clojure/tools.namespace "0.2.7"]
-   [aprint "0.1.0" :exclusions [org.clojure/clojure]]
-   #_[leiningen #=(leiningen.core.main/leiningen-version)  :exclusions [org.clojure/clojure]]
    [spyscope "0.1.4"  :exclusions [org.clojure/clojure]] ; tracing tools
-   [io.aviso/pretty "0.1.12"] ; better pretty printing
    [im.chit/iroh "0.1.11"  :exclusions [org.clojure/clojure]] ; java reflection tools
    [im.chit/vinyasa "0.2.2" :exclusions [org.clojure/clojure]] ; workflow tools
-   [slamhound "1.5.5"] ; namespace imports
-   [org.clojars.gjahad/debug-repl "0.3.3"] ; debug repl
-   ]
+                                        ;[slamhound "1.5.5"] ; namespace imports
+   [com.gfredericks/debug-repl "0.0.6"]]
 
   :injections
-  [(require 'spyscope.core)
-   (require '[vinyasa.inject :as inject])
-   (require 'io.aviso.repl)
-   (require 'alex-and-georges.debug-repl)
-   (load-file (str (System/getProperty "user.home") "/.lein/user.clj"))
-   (require 'user)
+  [(require '[vinyasa.inject :as inject])
+
+   (require 'com.gfredericks.debug-repl)
+   (load-file (str (System/getProperty "user.home") "/.lein/tracetool.clj"))
+                                        ;   (require 'user)
 
    (inject/in ;; the default injected namespace is `.`
-
     ;; note that `:refer, :all and :exclude can be used
     [vinyasa.inject :refer [inject [in inject-in]]]
-    #_[vinyasa.lein :exclude [*project*]]
-
-    [user :refer [trace trace-env]]
-
-    [alex-and-georges.debug-repl :refer [debug-repl exit-dr quit-dr view-locals local-bindings eval-with-locals]]
-
-    ;; imports all functions in vinyasa.pull
+    [tracetool :refer [trace trace-env *trace* defn-trace no-trace]]
+    [clojure.pprint :refer [pprint pp]]
+    [clojure.repl :refer [dir-fn doc source]]
+    [clojure.java.shell :refer [sh]]
+    [clojure.data :refer [diff]]
     [vinyasa.pull :all]
+    [com.gfredericks.debug-repl :refer [break! unbreak! unbreak]])
 
-
-    [cemerick.pomegranate add-classpath get-classpath resources]
-
-    [aprint.core :refer [aprint]]
-
-    ;; inject into clojure.core
-    clojure.core
-    [iroh.core .> .? .* .% .%>]
-
-    ;; inject into clojure.core with prefix
-    clojure.core >
-    [clojure.pprint pprint]
-    [clojure.pprint pp]
-    [clojure.java.shell sh])
-
-   ;; clearer exceptions/stack-traces
-
-   #_(alter-var-root #'clojure.main/repl-caught
-                     (constantly @#'io.aviso.repl/pretty-pst))
-   #_(io.aviso.repl/install-pretty-exceptions)
    ]
 
   :repl-options
-  {#_:nrepl-middleware #_[ io.aviso.nrepl/pretty-middleware]
-   :init
+  {:init
    (do
-     (use '[clojure.repl :only (dir-fn doc source)]))}}
-
-}
+     (use '[clojure.repl :only (dir-fn doc source)]))}}}
