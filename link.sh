f022cf14faaf5ce4f37aea89e8ed1cec4bbd79cf
Chris Blom <chris@Chris-MacBook.local>
2015-08-19 09:43:16 +0200

    Add hook to prevent force push to master or develop

--- link.sh
+++ link.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ln -s ${DIR}/config    ${HOME}/.config
 ln -s ${DIR}/lein      ${HOME}/.lein/
 ln -s ${DIR}/tmux.conf ${HOME}/.tmux.conf
-ln -s ${DIR}/gitconfig ${HOME}/.gitconfig
+ln -s ${DIR}/git/gitconfig ${HOME}/.gitconfig
