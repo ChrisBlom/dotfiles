43bdee499391ed3b869cba0ff004c6650cd39a4a
Chris Blom <chris.blom@millmobile.com>
2016-08-18 16:24:28 +0200

    Update leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,70 +1,60 @@
-{:profiles
 
- {:dev {:dependencies []}}
+{:jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; DONT . OMIT . STACKTRACES!!!
+
+ :profiles
+ {:dev
+  {:dependencies []}}
 
  :cider
  {:plugins
-  [[cider/cider-nrepl "0.9.1"] ]
+  [[cider/cider-nrepl "0.12.0"] ]
   :dependencies
-  [[org.clojure/tools.nrepl "0.2.7"]]
-  :jvm-opts ["-Dapple.awt.UIElement=true"] ; hide java icon in OSX dock
-  }
-
+  [[org.clojure/tools.nrepl "0.2.12"]]
+                                        ; hide java icon in OSX dock
+  :jvm-opts ["-Dapple.awt.UIElement=true"]}
 
  :check
  {:plugins
-  [[jonase/kibit "0.0.8"]
-   [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]}
-
+  [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]] ; static code analysis
+   [lein-cljfmt "0.5.3"]
+   ]}
 
  :power
- {
-  :plugins
-  [#_[com.gfredericks/nrepl-53-monkeypatch "0.1.0"]
-   [cider/cider-nrepl "0.9.1"] ; cider repl integration
-   [refactor-nrepl "1.0.5"]
-   ;; [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]] ; static code analysis
-   ;; [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]] ; linter
-   ;; [lein-ancient "0.5.5"  :exclusions [org.clojure/clojure]] ; dependency update checker
+ {:plugins
+  [[cider/cider-nrepl "0.12.0"] ; cider repl integration
+   [refactor-nrepl "2.2.0"]
+                                        ;[lein-ring "0.9.7"]
+   [lein-ancient "0.6.10"  :exclusions [org.clojure/clojure]] ; dependency update checker
    ;; [lein-localrepo "0.5.3" :exclusions [org.clojure/clojure]] ; installs artifacts in local maven repo
+   [lein-localrepo "0.5.3"]
+   [dep-graph "0.1.0"]
    ]
 
-  :jvm-opts ["-Dapple.awt.UIElement=true"] ; hide java icon in OSX dock
+  :jvm-opts ["-Dapple.awt.UIElement=true"  ; hide java icon in OSX dock
+             "-XX:-OmitStackTraceInFastThrow"  ; DONT . OMIT . STACKTRACES!!!
+             ]
 
   :dependencies
-  [[org.clojure/tools.nrepl "0.2.7"]
-   [org.clojure/tools.namespace "0.2.7"]
-   ;[pjstadig/humane-test-output "0.7.0"]
-   #_[spyscope "0.1.4"  :exclusions [org.clojure/clojure]] ; tracing tools
-   #_[im.chit/iroh "0.1.11"  :exclusions [org.clojure/clojure]] ; java reflection tools
-   [im.chit/vinyasa "0.2.2" :exclusions [org.clojure/clojure]] ; workflow tools
-   #_[com.gfredericks/debug-repl "0.0.6"]]
-
+  [[org.clojure/tools.nrepl "0.2.12"]
+   [acyclic/squiggly-clojure "0.1.6"] ; linters for emacs
+   [org.clojure/tools.namespace "0.2.10"]
+   [im.chit/vinyasa "0.4.2" :exclusions [org.clojure/clojure]] ; workflow tools
+   [alembic "0.3.2"]]
 
   :injections
   [(require '[vinyasa.inject :as inject])
-
+   (require 'alembic.still)
    (load-file (str (System/getProperty "user.home") "/.lein/tracetool.clj"))
-                                        ;   (require 'user)
 
-   ;; (require 'pjstadig.humane-test-output)
-   ;; (pjstadig.humane-test-output/activate!)
-
+   ;; inject utility fns in the '.' namespace
    (inject/in ;; the default injected namespace is `.`
-    ;; note that `:refer, :all and :exclude can be used
     [vinyasa.inject :refer [inject [in inject-in]]]
-    [tracetool :refer [trace trace-env *trace* defn-trace no-trace]]
+    [tracetool :refer [trace trace-env *trace* defn-trace no-trace ll undef clear-ns pid diff time]]
     [clojure.pprint :refer [pprint pp]]
     [clojure.repl :refer [dir-fn doc source]]
     [clojure.java.shell :refer [sh]]
-    [clojure.data :refer [diff]]
-    [vinyasa.pull :all]
-    #_[com.gfredericks.debug-repl :refer [break! unbreak! unbreak!!]])
-
-   ]
+    [alembic.still :refer [distill lein load-project]])]
 
   :repl-options
-  {:init
-   (use '[clojure.repl :only (dir-fn doc source)])
-   :nrepl-middleware
-   [#_com.gfredericks.debug-repl/wrap-debug-repl]}}}
+  {:init (use '[clojure.repl :only (dir-fn doc source)])}}}
