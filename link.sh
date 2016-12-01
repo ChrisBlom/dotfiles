ab741d86d2db84fd6901cd12ff33f4fa1cf7a53f
Chris Blom <chris.blom@millmobile.com>
2016-12-01 13:48:34 +0100

    Add configs for curl, csi, and dockermachine

--- link.sh
+++ link.sh
@@ -6,3 +6,6 @@ ln -s ${DIR}/config    ${HOME}/.config
 ln -s ${DIR}/lein      ${HOME}/.lein/
 ln -s ${DIR}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${DIR}/git/gitconfig ${HOME}/.gitconfig
+ln -s ${DIR}/curlrc ${HOME}/.curlrc
+ln -s ${DIR}/csirc ${HOME}/.csirc
+ln -s ${DIR}/com.docker.machine.default.plist ~/Library/LaunchAgents/com.docker.machine.default.plist
