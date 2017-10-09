d5214f7f9a0c54d11ba29ac9f2cf46fccafd2def
Chris Blom <chris.blom@millmobile.com>
2017-10-09 10:22:21 +0200

    Update lein profile.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -18,14 +18,14 @@
   [[jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
    [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]] ; static code analysis
    [lein-cljfmt "0.5.3"]
-   ]}
+   [lein-ancient "0.6.10"  :exclusions [org.clojure/clojure]] ; dependency update checker
+   [lein-cljfmt "0.5.3"]]}
 
  :power
  {:plugins
   [[cider/cider-nrepl "0.14.0"] ; cider repl integration
-   [refactor-nrepl "2.3.0-SNAPSHOT"]
-   [lein-ancient "0.6.10"  :exclusions [org.clojure/clojure]] ; dependency update checker
-   ]
+   ;[com.billpiel/sayid "0.0.14" :exclusions [org.clojure/tools.namespace]]
+   [refactor-nrepl "2.3.0-SNAPSHOT"]]
 
   :jvm-opts ["-Dapple.awt.UIElement=true"  ; hide java icon in OSX dock
              "-XX:-OmitStackTraceInFastThrow"  ; DONT . OMIT . STACKTRACES!!!
@@ -33,27 +33,14 @@
 
   :dependencies
   [[org.clojure/tools.nrepl "0.2.12"]
-   [acyclic/squiggly-clojure "0.1.6"] ; linters for emacs
-   [org.clojure/tools.namespace "0.2.10"]
-   [im.chit/vinyasa "0.4.2" :exclusions [org.clojure/clojure]] ; workflow tools
-   [alembic "0.3.2"]]
+   ;; [acyclic/squiggly-clojure "0.1.6"] ; linters for emacs
+   ;;[org.clojure/tools.namespace "0.2.10"]
+   ]
 
   :injections
-  [(require '[vinyasa.inject :as inject])
-   (require 'alembic.still)
-   (load-file (str (System/getProperty "user.home") "/.lein/tracetool.clj"))
-   ;; inject utility fns in the '.' namespace
-   (inject/in ;; the default injected namespace is `.`
-    [vinyasa.inject :refer [inject [in inject-in]]]
-    [tracetool :refer [trace trace-env *trace* defn-trace no-trace ll undef clear-ns pid diff
-                       time! toggle-timed!
-                       clear-times untime-all
-                       ;; capture instrument uninstrument instrument-report
-                       ]]
-    [clojure.pprint :refer [pprint pp]]
-    [clojure.repl :refer [dir-fn doc source]]
-    [clojure.java.shell :refer [sh]]
-    [alembic.still :refer [distill lein load-project]])]
+  [(load-file (str (System/getProperty "user.home") "/.lein/..clj"))
+   (require '.)]
 
   :repl-options
-  {:init (use '[clojure.repl :only (dir-fn doc source)])}}}
+  {:timeout 120000
+   :init (use '[clojure.repl :only (dir-fn doc source)])}}}
